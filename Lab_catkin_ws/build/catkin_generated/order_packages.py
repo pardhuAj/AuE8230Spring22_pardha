51854d199ba460dad0807362bcc74abd36834ec8
asura <pardha.s.krishna@outlook.com>
2022-02-05 16:41:32 -0500

    Uploading the lab folders

---
+++ Lab_catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/asura/Lab_catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/asura/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/asura/catkin_ws/devel;/opt/ros/noetic' != '' else []
